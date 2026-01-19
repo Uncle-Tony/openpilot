52fc0aa27b07e9854dfd2c6e998936a509db6cc7
Uncle-Tony <Uncle-Tony@users.noreply.github.com>
2026-01-19 13:57:06 -0600

    rivian: change engagement check to ACM_FeatureStatus == 2

--- opendbc_repo/opendbc/safety/modes/rivian.h
+++ opendbc_repo/opendbc/safety/modes/rivian.h
@@ -101,7 +101,7 @@ static void rivian_rx_hook(const CANPacket_t *msg) {
     // Cruise state
     if (msg->addr == 0x100U) {
       const int feature_status = msg->data[2] >> 5U;
-      pcm_cruise_check(feature_status == 1);
+      pcm_cruise_check(feature_status == 2);
     }
   }
 }
